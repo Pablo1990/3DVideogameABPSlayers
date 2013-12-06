12043e12cfd2a7eeb5186c7d396373ec6103888c
Pablo Vicente Munuera <pablovm1990@gmail.com>
2013-12-06 12:18:04 +0100

    Arreglado un gran error, en breves de funcionar

--- fde/main.cpp
+++ fde/main.cpp
@@ -2,9 +2,10 @@
 #include <cfiredoor.h>
 #include <iostream>
 #include <stdlib.h>
+#include <SFML/Graphics.hpp>
 
 #define N 10
-#define PASADAS 10
+#define PASADAS 100
 #define DIMENSIONES 1
 
 using namespace FireDoorEscaper;
@@ -26,6 +27,45 @@ printGameStatus(const CGame& g) {
     for (unsigned i=0; i < inp.size(); i++)
         std::cout << inp[i] << " ";
     std::cout << "] \n";
+}
+
+void pintarActual(double inputs[N][DIMENSIONES], bool onfireInputs[N], double pesos[DIMENSIONES+1]){
+
+    sf::RenderWindow window(sf::VideoMode(300, N*10), "Traza");
+    window.clear();
+
+    for (int i = 0; i < N; ++i)
+    {
+        sf::CircleShape shape(2.f);
+        if(onfireInputs[i])
+        {
+            shape.setFillColor(sf::Color::Red);
+        }
+        else
+        {
+            shape.setFillColor(sf::Color::Blue);
+        }
+        shape.setPosition(inputs[i][0]+100, i*3);
+        window.draw(shape);
+    }
+
+    sf::RectangleShape rectangle;
+    rectangle.setSize(sf::Vector2f(2, 200));
+    rectangle.setOutlineColor(sf::Color::Green);
+    rectangle.setPosition(pesos[1], pesos[0]);
+    window.draw(rectangle);
+    std::cout<<"pesos "<< pesos[0]<<", " <<pesos[1]<<"\n"; 
+
+    window.display();
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
 }
 
 bool h(double pesos[DIMENSIONES+1], double x[DIMENSIONES])
@@ -103,6 +143,9 @@ bool training(double inputs[N][DIMENSIONES], bool onfireInputs[N], double valorA
         {
             break;
         }
+
+
+        pintarActual(inputs, onfireInputs, pesos);
     }
 
     return h(pesos, valorAPredecir);
@@ -114,10 +157,10 @@ main(void) {
     // Use GDM_LEVELUP for increasing level of difficulty
     CGame *game = new CGame(0);
     game->setGameDifficultyMode(CGame::GDM_SAMELEVEL);
-    const CFireDoor& fd = game->getCurrentFireDoor();
-    const CFireDoor::TVecDoubles& inp = fd.getNextStepInputs();
     double inputs[N][DIMENSIONES];
-    bool onfireInputs[N];    
+    bool onfireInputs[N];
+
+    
 
     // Main loop: stay will the game is on (i.e. the player is alive)
     while (game->getGameStatus() == CGame::GS_PLAYING) {
@@ -127,16 +170,19 @@ main(void) {
             game->nextStep();
 
             const CFireDoor& fd1 = game->getCurrentFireDoor();
-            const CFireDoor::TVecDoubles& inp1 = fd.getNextStepInputs();
+            const CFireDoor::TVecDoubles& inp1 = fd1.getNextStepInputs();
             for(int j=0; j<DIMENSIONES; j++)
             {
-                inputs[i][j]=inp[i];   
+                inputs[i][j]=inp1[j];   
             }
-            onfireInputs[i] = fd.isOnFire();
+            onfireInputs[i] = fd1.isOnFire();
         }
         // Try to cross the current FireDoor
         printGameStatus(*game);
         std::cout << "**** TRYING TO CROSS THE DOOR ****\n";
+
+        const CFireDoor& fd = game->getCurrentFireDoor();
+        const CFireDoor::TVecDoubles& inp = fd.getNextStepInputs();
         double valorAPredecir[DIMENSIONES];
         for (int j = 0; j < DIMENSIONES; ++j)
         {
