4c01915aa9567c86738ae1d0949e5eb22cf9c10b
Pablo Sánchez <pasaloray@hotmail.com>
2013-12-10 10:23:35 +0100

    Más cambios en la identación.

--- fde/main.cpp
+++ fde/main.cpp
@@ -29,22 +29,18 @@ printGameStatus(const CGame& g) {
     std::cout << "] \n";
 }
 
-void pintarActual(double inputs[N][DIMENSIONES], bool onfireInputs[N], double pesos[DIMENSIONES+1]){
-
+void pintarActual(double inputs[N][DIMENSIONES], bool onfireInputs[N], double pesos[DIMENSIONES+1]) {
     sf::RenderWindow window(sf::VideoMode(300, N*10), "Traza");
     window.clear();
 
-    for (int i = 0; i < N; ++i)
-    {
+    for (int i = 0; i < N; ++i) {
         sf::CircleShape shape(2.f);
+        
         if (onfireInputs[i])
-        {
             shape.setFillColor(sf::Color::Red);
-        }
         else
-        {
             shape.setFillColor(sf::Color::Blue);
-        }
+        
         shape.setPosition(inputs[i][0]+100, i*3);
         window.draw(shape);
     }
@@ -57,95 +53,73 @@ void pintarActual(double inputs[N][DIMENSIONES], bool onfireInputs[N], double pe
     std::cout<<"pesos "<< pesos[0]<<", " <<pesos[1]<<"\n"; 
 
     window.display();
-    while (window.isOpen())
-    {
+    
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
     }
 }
 
-bool h(double pesos[DIMENSIONES+1], double x[DIMENSIONES])
-{
+bool h(double pesos[DIMENSIONES+1], double x[DIMENSIONES]) {
     double suma=pesos[0];
     for (int i = 1; i < DIMENSIONES+1; ++i)
-    {
-        suma+=pesos[i]*x[i-1];
-    }
+        suma += pesos[i]*x[i-1];
+
     return suma>=0;
 }
 
-void actualizarPesos(double pesos[DIMENSIONES+1], double inputs[DIMENSIONES], double correct)
-{
-    pesos[0]+=correct;
+void actualizarPesos(double pesos[DIMENSIONES+1], double inputs[DIMENSIONES], double correct) {
+    pesos[0] += correct;
+    
     for (int i = 1; i < DIMENSIONES+1; ++i)
-    {
-        pesos[i]+=correct*inputs[i-1];
-    }
+        pesos[i] += correct*inputs[i-1];
 }
 
-bool training(double inputs[N][DIMENSIONES], bool onfireInputs[N], double valorAPredecir[DIMENSIONES])
-{
-    int count=0;
+bool training(double inputs[N][DIMENSIONES], bool onfireInputs[N], double valorAPredecir[DIMENSIONES]) {
+    int count = 0;
     double errores[N];
     double pesos[DIMENSIONES+1];
-    int erroresCount=0;
-    double valorCritico=0;
+    int erroresCount = 0;
+    double valorCritico = 0;
     
-    pesos[0]=1;     // Se inicializa con peso=1 porque es el threshold
+    pesos[0] = 1;     // Se inicializa con peso=1 porque es el threshold
     
     for (int i = 1; i <= DIMENSIONES; ++i)
-    {
         pesos[i] = 0;
-    }
-    int k=0;
-
-    while (count<PASADAS)
-    {
+    
+    while (count<PASADAS) {
         count++;
-        erroresCount=0;
+        erroresCount = 0;
 
-        for (int i = 0; i < N; ++i)
-        {
-            if (h(pesos,inputs[i]))
-            {
-                if (onfireInputs[i]) //debería estar not on fire
-                {
-                    errores[erroresCount]=i;
+        for (int i = 0; i < N; ++i) {
+            if (h(pesos,inputs[i])) {
+                if (onfireInputs[i]) { //debería estar not on fire
+                    errores[erroresCount] = i;
                     erroresCount++;
                 }
             }
-            else
-            {
-                if (!onfireInputs[i]) //debería estar on fire
-                {
-                    errores[erroresCount]=i;
+            else {
+                if (!onfireInputs[i]) { //debería estar on fire
+                    errores[erroresCount] = i;
                     erroresCount++;
                 }
             }
         }
 
         //cogemos otro valor para ver que pasa
-        if (erroresCount>0)
-        {
+        if (erroresCount>0) {
             int i = errores[rand()%erroresCount];
+            
             if (!onfireInputs[i])
-            {
                 actualizarPesos(pesos, inputs[i], 1);
-            }
             else
-            {
                 actualizarPesos(pesos, inputs[i], -1);
-            }
         }
         else
-        {
             break;
-        }
-
 
         pintarActual(inputs, onfireInputs, pesos);
     }
@@ -162,8 +136,6 @@ main(void) {
     double inputs[N][DIMENSIONES];
     bool onfireInputs[N];
 
-    
-
     // Main loop: stay will the game is on (i.e. the player is alive)
     while (game->getGameStatus() == CGame::GS_PLAYING) {
     // Do some game steps and print values
@@ -173,9 +145,7 @@ main(void) {
                     game->getCurrentFireDoor().getNextStepInputs();
 
             for(int j=0; j<DIMENSIONES; j++)
-            {
-                inputs[i][j]=inp1[j];   
-            }            
+                inputs[i][j] = inp1[j];   
 
             game->nextStep();
 
@@ -188,14 +158,13 @@ main(void) {
         const CFireDoor& fd = game->getCurrentFireDoor();
         const CFireDoor::TVecDoubles& inp = fd.getNextStepInputs();
         double valorAPredecir[DIMENSIONES];
+        
         for (int j = 0; j < DIMENSIONES; ++j)
-        {
             valorAPredecir[j] = inp[j];
-        }
+
         if (training(inputs, onfireInputs, valorAPredecir))
-        {
             game->crossFireDoor();
-        }        
+
         if (game->getGameStatus() != CGame::GS_PLAYING)
             std::cout << "!!!!!!!!!!! PLAYER GOT BURNED OUT !!!!!!!!!!!!!!\n";
         else
