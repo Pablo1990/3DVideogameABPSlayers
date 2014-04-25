e64ac4779b9c63662e6fc58ff4dadc776fa673a0
Pablo Vicente Munuera <pablovm1990@gmail.com>
2014-04-25 11:51:43 +0200

    anyadida red al goal behaviour

---
+++ GoalBehavior/GoalBehavior/aprendizaje/xor.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <math.h>
+
+#include "CNeuralNet.h"
+#include "utils.h"
+#include "C2DMatrix.h"
+#include "SVector2D.h"
+#include "CParams.h"
+
+using namespace std;
+
+class xor
+{
+public:
+	xor(void);
+	~xor(void);
+	bool Update();
+	void Reset();
+	int getDigito1();
+	int getDigito2();
+	double getResultado();
+	double Fitness();
+	void PutWeights(vector<double> &w){m_ItsBrain.PutWeights(w);}
+	int GetNumberOfWeights()const{return m_ItsBrain.GetNumberOfWeights();}
+	void aumentoFitness() {fitness++;}
+
+private:
+	CNeuralNet m_ItsBrain;
+	int digito1;
+	int digito2;
+	double resultado;
+	double fitness;
+};
+
